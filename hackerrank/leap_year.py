7ebbb305921b00a4d9ba8566b77dc69b071fab99
Dray <vel_luxe@yahoo.com>
2020-11-16 16:43:09 +0800

    change leap to return

--- hackerrank/leap_year.py
+++ hackerrank/leap_year.py
@@ -1,12 +1,12 @@
 def is_leap(year):
     
   if year % 4 == 0 and year % 100 != 0: 
-      leap = True
+      return True
   elif year % 100 == 0 and year % 400 != 0:
-      leap = False
+      return False
   elif year % 400 == 0:
-      leap = True
+      return True
   else:
-      leap = False
+      return False
 
 year = int(input())
