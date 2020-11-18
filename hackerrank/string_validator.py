bf677db3dbe9a9cafe7b0a6f4f9eb0866efc33f3
Dray <adriel_sanmillan@yahoo.copm>
2020-11-18 14:37:21 +0800

    fix string_validator

--- hackerrank/string_validator.py
+++ hackerrank/string_validator.py
@@ -1,17 +1,6 @@
 s = input()
-check = []
-for _ in range(5):
-    if s.isalnum() == True:
-        check.append(1)
-    elif s.isalpha() == True:
-        check.append(1)
-    elif s.isdigit() == True:
-        check.append(1)
-    elif s.islower() == True:
-        check.append(1)
-    elif s.isupper() == True:
-        check.append(1)
-    else:
-        check.append(0)
-for x in check:
-    print(True if 1 else False)
+print(True) if s.isalnum() else print(False)
+print(True) if s.isalpha() else print(False)
+print(True) if s.isdigit() else print(False)
+print(True) if s.islower() else print(False)
+print(True) if s.isupper() else print(False)
