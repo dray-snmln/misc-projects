11131786862943858c5853068218e474f6066caf
Dray <adriel_sanmillan@yahoo.copm>
2020-12-03 19:17:17 +0800

    codewars

---
+++ hackerrank/vowel_count.py
@@ -0,0 +1,9 @@
+def get_count(input_str):
+    num_vowels = 0
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in input_str:
+        if i in vowels:
+            num_vowels
+    
+    return num_vowels
+get_count("atari")
